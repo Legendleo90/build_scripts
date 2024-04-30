c7d34ebb007bf0a0869d469220a8ce8436ecbf17
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-04-30 06:02:29 +0530

    build: Update script

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#removals
+rm -rf .repo/local_manifests
+
 #sync
 repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
 git clone https://github.com/Legendleo90/local_manifests.git -b pos .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build
+/opt/crave/resync.sh
 . build/envsetup.sh
 lunch aosp_beryllium-ap1a-user
+make installclean
 make bacon
