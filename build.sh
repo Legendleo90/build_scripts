93e07bc2e9aab5c3e4f9c33d17ed2246d7bf9d00
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-05-08 00:38:00 +0530

    scripts: Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ rm -rf .repo/local_manifests
 
 #sync
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
+git clone https://github.com/Legendleo90/local_manifests.git -b rising .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 #ksu
