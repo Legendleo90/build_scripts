f88376ed99d493fec2031bb6a6668acbb72f941e
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-05-31 20:35:28 +0530

    build.sh: Improve build script

--- build.sh
+++ build.sh
@@ -1,18 +1,29 @@
-#!/bin/bash
-
-#removals
+# Removals
 rm -rf .repo/local_manifests
 
-#sync
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
-git clone https://github.com/Legendleo90/local_manifests.git -b rising .repo/local_manifests
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+# Initialize repo with specified manifest
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1 
 
-#ksu
+# Clone local_manifests repository
+git clone https://github.com/Legendleo90/local_manifests --depth 1 -b rising .repo/local_manifests 
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+# KSU
 cd kernel/xiaomi/beryllium && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
 
-# build
-. build/envsetup.sh
+# Clone Private Keys
+git clone https://github.com/Legendleo90/private_keys.git -b main vendor/rising/signing/keys
+
+# Set up build environment
+source build/envsetup.sh
+
+# Lunch configuration
 riseup beryllium user
 make installclean
-rise b
+
+# Build
+croot
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout' 
+rise sb
