bc0bdc23c2490a3a4d30d603c621b36abb7b03bf
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-06-04 22:22:15 +0530

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 # Removals
 rm -rf .repo/local_manifests
-rm -rf external
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1 
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
 
 # Clone local_manifests repository
 git clone https://github.com/Legendleo90/local_manifests --depth 1 -b rising .repo/local_manifests
 
 # Sync the repositories
-/opt/crave/resync.sh
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
 
-# KSU
-cd kernel/xiaomi/beryllium && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
+# Custom Repos
+rm -rf build/make && git clone https://github.com/RisingOS-staging/android_build build/make --depth=1
+rm -rf vendor/rising && git clone https://github.com/RisingOS-staging/android_vendor_rising vendor/rising --depth=1
 
-# Clone Private Keys
-git clone https://github.com/Legendleo90/private_keys.git -b main vendor/rising/signing/keys
+# Private Keys
+rm -rf vendor/lineage-priv && git clone https://github.com/Legendleo90/vendor_lineage-priv vendor/lineage-priv
 
 # Set up build environment
 source build/envsetup.sh
@@ -26,5 +30,5 @@ make installclean
 
 # Build
 croot
-repo forall -c 'git lfs install && git lfs pull && git lfs checkout' 
-rise sb
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
+rise b
