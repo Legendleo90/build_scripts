d438b891175f11ae56e6835ee010c2a38b02c7ba
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-06-15 20:34:12 +0530

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ git clone https://github.com/Legendleo90/local_manifests --depth 1 -b los .repo/
 # Sync the repositories
 /opt/crave/resync.sh
 
+# Cherry-Picks
+cd f*/b* && git fetch https://github.com/Legendleo90/android_frameworks_base && git cherry-pick 64b309df6a5a834bbc5bab48b918e5dd8b944016 && cd ../..
+
 # Private Keys
 git clone https://github.com/Legendleo90/vendor_lineage-priv -b los vendor/evolution-priv
 
