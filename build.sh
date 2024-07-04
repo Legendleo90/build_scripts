64aeacf4e976ecd6ce871f883fbab808299f6056
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-07-04 18:21:43 +0530

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 # Removals
-rm -rf .repo/local_manifests
+device/xiaomi/beryllium
 
 # Initialize repo with specified manifest
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
 
-# Clone local_manifests repository
-git clone https://github.com/Legendleo90/local_manifests --depth 1 -b rising .repo/local_manifests
+# Clone device tree
+git clone https://github.com/Legendleo90/device_xiaomi_beryllium -b rising device/xiaomi/beryllium
 
 # Sync the repositories
 /opt/crave/resync.sh
 
-# Custom Repos
-rm -rf frameworks/base && git clone https://github.com/Legendleo90/rising_fwb frameworks/base --depth=1
+# Cherry-picks
+cd frameworks/base && git fetch https://github.com/Legendleo90/android_frameworks_base && git cherry-pick 64b309df6a5a834bbc5bab48b918e5dd8b944016 && cd ../..
 
 # Private Keys
 rm -rf vendor/lineage-priv && git clone https://github.com/Legendleo90/vendor_lineage-priv vendor/lineage-priv
@@ -23,7 +23,7 @@ source build/envsetup.sh
 riseup beryllium user
 
 # Cleanup directories
-# make installclean
+make installclean
 
 # Git-lfs
 repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
