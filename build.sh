5c59cd9947f4b4b1e7e0037bfae1ff7e4ffee2a4
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-08-01 10:51:00 +0530

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ rm -rf vendor/aospa
 rm -rf vendor/aospa/prebuilt
 rm -rf packages/apps/Launcher3
 rm -rf frameworks/base
+rm -rf frameworks/av
 rm -rf packages/apps/Settings
 
 # Initialize repo with specified manifest
@@ -17,14 +18,20 @@ git clone https://github.com/Legendleo90/device_xiaomi_beryllium-clo -b uvite de
 
 # Customs
 rm -rf frameworks/base && git clone https://github.com/Oneplus-6T/aospa_fwb -b uvite frameworks/base --depth=1
+rm -rf frameworks/av && git clone https://github.com/Oneplus-6T/aospa_av -b uvite frameworks/av --depth=1
 rm -rf packages/apps/Settings && git clone https://github.com/Oneplus-6T/aospa_Settings -b uvite packages/apps/Settings --depth=1
 rm -rf vendor/aospa && git clone https://github.com/Oneplus-6T/aospa_vendor -b uvite vendor/aospa --depth=1
 rm -rf vendor/aospa/prebuilt && git clone https://gitlab.com/ThankYouMario/android_vendor_aospa_prebuilt -b uvite vendor/aospa/prebuilt --depth=1
-rm -rf packages/apps/Launcher3 && git clone https://github.com/Oneplus-6T/aospa_Launcher3 -b unity packages/apps/Launcher3 --depth=1
 rm -rf packages/apps/GameSpace && git clone https://github.com/Oneplus-6T/GameSpace -b fourteen packages/apps/GameSpace --depth=1
 
 # Private Keys
 rm -rf ~/.android-certs && git clone https://github.com/Legendleo90/private_keys ~/.android-certs
 
+# Set up build environment
+source build/envsetup.sh
+
+# Cleanup directories
+make installclean
+
 # Build
-./rom-build.sh beryllium -c -t user -s ~/.android-certs
+./rom-build.sh beryllium -t user -s ~/.android-certs
