77224f671b491db29359dab3b0e6883bc45736d0
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-08-17 21:54:10 +0530

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ rm -rf device/xiaomi/beryllium
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
 
 # Clone device tree
-git clone https://github.com/Legendleo90/device_xiaomi_beryllium-4.19 -b fourteen-inf device/xiaomi/beryllium
+git clone https://github.com/Legendleo90/device_xiaomi_beryllium-4.19 -b fourteen device/xiaomi/beryllium
 
 # Sync the repositories
 /opt/crave/resync.sh
 
 # Private Keys
-rm -rf vendor/infinity-priv && git clone https://github.com/Legendleo90/vendor_lineage-priv -b inf vendor/infinity-priv
+rm -rf vendor/infinity-priv && git clone https://github.com/Legendleo90/vendor_lineage-priv -b infinity vendor/infinity-priv
 
 # Set up build environment
 source build/envsetup.sh
