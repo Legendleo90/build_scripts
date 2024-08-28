cd96594f7b8e1601fffb1c695351310d07bfcc02
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2024-08-28 12:18:19 +0530

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -1,23 +1,25 @@
 # Removals
 rm -rf device/xiaomi/beryllium
+rm -rf frameworks/native
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/ProjectEverest/manifest -b 14 --git-lfs --depth=1
 
 # Clone device tree
-git clone https://github.com/Legendleo90/device_xiaomi_beryllium -b rising device/xiaomi/beryllium
+git clone https://github.com/ProjectEverest-Devices/android_device_xiaomi_beryllium -b 14 device/xiaomi/beryllium
 
 # Sync the repositories
 /opt/crave/resync.sh && /opt/crave/resync.sh
 
 # Private Keys
-rm -rf vendor/lineage-priv && git clone https://github.com/Legendleo90/vendor_lineage-priv vendor/lineage-priv
+rm -rf .android-certs && git clone https://github.com/Legendleo90/private_keys .android-certs
+cp .android-certs/* vendor/lineage/signing/keys/
 
 # Set up build environment
 source build/envsetup.sh
 
 # Lunch configuration
-riseup beryllium user
+lunch lineage_beryllium-user
 
 # Cleanup directories
 make installclean
@@ -26,4 +28,4 @@ make installclean
 repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
 
 # Build
-rise b
+mka everest
